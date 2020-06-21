c0a7a38b28c636dc0684b7205cdbc9c8461cb216
SoozaV <s8za.92@gmail.com>
2020-06-21 02:43:46 -0500

    Agregando estilos al ejercicio 3
    Se agregan estilos al ejercicio 3, se corrigen algunos errores del ejercicio 1 y se elimina el ejercicio 2.

--- Ejercicio 02 - Alert 2/codigo.js
+++
@@ -1,8 +0,0 @@
-var mensaje = "Hola Mundo!\nQué fácil es incluir \'comillas simples\' y \"comillas dobles\"";
-
-
-//Se ejecuta el primer alert
-alert(mensaje);
-
-//Se ejecuta el segundo alert después de dar clic en 'aceptar' en el primero
-alert("Soy el primer script");
--- Ejercicio 03 - Array/codigo.js
+++ Ejercicio 03 - Array/codigo.js
@@ -1,3 +1,38 @@
-var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+// Se obtienen los elementos del DOM
+var boton = document.getElementById("meses"),
+    numeroMes = document.getElementById("numeroMes"),
+    ejercicio = document.getElementById("ejercicio"),
+    codigo = document.getElementById("codigo"),
+    mostrarEjercicioBtn = document.getElementById("mostrarEjercicio"),
+    mostrarCodigoBtn = document.getElementById("mostrarCodigo");
 
-alert(meses.join(', '));
+// Array que almacena los meses del año.
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+             "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// Función para cambiar la clase del <div> ejercicio.
+function mostrarEjercicio(){
+    ejercicio.classList.toggle("mostrar");
+    ejercicio.classList.contains("mostrar") ? mostrarEjercicioBtn.innerHTML = "Ocultar instrucciones" : mostrarEjercicioBtn.innerHTML = "Mostrar instrucciones";
+}
+
+// Función para cambiar la clase del <div> código.
+function mostrarCodigo(){
+    codigo.classList.toggle("mostrar");
+    codigo.classList.contains("mostrar") ? mostrarCodigoBtn.innerHTML = "Ocultar código" : mostrarCodigoBtn.innerHTML = "Mostrar código";
+}
+
+// Función para mostrar los mensajes de alerta.
+function mostrarMeses(){
+    if(numeroMes.value == ""){
+        alert(meses.join(', '));
+    } else if(numeroMes.value < 1 || numeroMes.value > 12 || numeroMes.value % 1 !== 0){
+        alert("Por favor introduce solo números enteros del 1 al 12.");
+    } else {
+        alert("El mes " + numeroMes.value + " es " + meses[numeroMes.value - 1]);
+    }
+}
+
+boton.onclick = mostrarMeses;
+mostrarEjercicioBtn.onclick = mostrarEjercicio;
+mostrarCodigoBtn.onclick = mostrarCodigo;
