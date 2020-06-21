0e117f7e3bece11d5cfbd9fc7ecbf034015f9425
SoozaV <s8za.92@gmail.com>
2020-06-21 00:35:53 -0500

    Estilos Ejercicio 1
    Se agregan estilos y se limpia un poco el código del ejercicio 1

--- Ejercicio 01 - Alert/codigo.js
+++ Ejercicio 01 - Alert/codigo.js
@@ -1,7 +1,34 @@
+// Se obtienen los elementos del DOM
+var boton = document.getElementById("alert");
+var ejercicio = document.getElementById("ejercicio");
+var codigo = document.getElementById("codigo");
+var mostrarEjercicioBtn = document.getElementById("mostrarEjercicio");
+var mostrarCodigoBtn = document.getElementById("mostrarCodigo");
+
+// Mensaje a mostrar en el alert. 
+// \n es para mostrar un salto de línea
+// \' sirve para mostrar comillas simples
+// \" sirve para mostrar comillas dobles
 var mensaje = "Hola Mundo!\nQué fácil es incluir \'comillas simples\' y \"comillas dobles\"";
 
-//Se ejecuta el primer alert
-alert(mensaje);
+// Función para cambiar la clase del <div> ejercicio.
+function mostrarEjercicio(){
+    ejercicio.classList.toggle("mostrar");
+    ejercicio.classList.contains("mostrar") ? mostrarEjercicioBtn.innerHTML = "Ocultar instrucciones" : mostrarEjercicioBtn.innerHTML = "Mostrar instrucciones";
+}
 
-//Se ejecuta el segundo alert después de dar clic en 'aceptar' en el primero
-alert("Soy el primer script");
+// Función para cambiar la clase del <div> código.
+function mostrarCodigo(){
+    codigo.classList.toggle("mostrar");
+    codigo.classList.contains("mostrar") ? mostrarCodigoBtn.innerHTML = "Ocultar código" : mostrarCodigoBtn.innerHTML = "Mostrar código";
+}
+
+// Función para mostrar los mensajes de alerta.
+function alerta(){
+    alert(mensaje);
+    alert("Soy el primer script");
+}
+
+boton.onclick = alerta;
+mostrarEjercicioBtn.onclick = mostrarEjercicio;
+mostrarCodigoBtn.onclick = mostrarCodigo;
